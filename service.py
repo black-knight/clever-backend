3b0d18c313695cbc7bae584946d45c0e2baa660e
Daniel Andersen <daniel.andersen@alexandra.dk>
2014-08-12 08:17:37 +0200

    Refresh of POI from webservice. Database connection.

--- service.py
+++ service.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, make_response
-from poi import start_poi_fetching_thread, get_cached_poi
+from poi import get_cached_poi, update_poi, initialize
 
 app = Flask(__name__)
 
@@ -7,12 +7,16 @@ app = Flask(__name__)
 def poi():
     return jsonify(get_cached_poi())
 
+@app.route('/clever/api/v1.0/refresh', methods = ['GET'])
+def refresh():
+    return jsonify(update_poi())
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify( { 'error': 'Not found' } ), 404)
 
-start_poi_fetching_thread()
+initialize()
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True, port=8083, use_reloader=False)
 
